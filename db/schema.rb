3ffc39a4331857149ca77f9696a6fc0014302559
Sunny Ripert <negatif@gmail.com>
2008-08-13 22:44:17 +0200

    Schema.rb updated to include Bills

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20080813203624) do
+
+  create_table "bills", :force => true do |t|
+    t.string   "title"
+    t.float    "amount"
+    t.integer  "type"
+    t.integer  "from_user_id"
+    t.integer  "to_user_id"
+    t.date     "transaction_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
