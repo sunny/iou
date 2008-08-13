67479b5cefae52c2b8a10cd8d54cd1e8603e992b
Sunny Ripert <negatif@gmail.com>
2008-08-13 23:17:19 +0200

    Added bill associations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,4 @@
 class Bill < ActiveRecord::Base
+  belongs_to :from_user, :class_name => 'User', :foreign_key => 'from_user_id'
+  belongs_to :to_user,   :class_name => 'User', :foreign_key => 'to_user_id'
 end
