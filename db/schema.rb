3826eb19b3c4ed6da99bd333465c0481d29ab5e4
Sunny Ripert <negatif@gmail.com>
2008-08-13 22:46:39 +0200

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080813203624) do
+ActiveRecord::Schema.define(:version => 20080813204551) do
 
   create_table "bills", :force => true do |t|
     t.string   "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(:version => 20080813203624) do
     t.integer  "from_user_id"
     t.integer  "to_user_id"
     t.date     "transaction_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
