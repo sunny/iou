834225a6d8961d76fe7a53895ee24774851c385e
Sunny <negatif@gmail.com>
2008-08-17 19:07:39 +0200

    Small changes to Bill model and more model validations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,7 +2,10 @@ class Bill < ActiveRecord::Base
   belongs_to :from_user, :class_name => 'User', :foreign_key => 'from_user_id'
   belongs_to :to_user,   :class_name => 'User', :foreign_key => 'to_user_id'
 
-  validates_numericality_of :amount, :greater_than => 0
+  validates_inclusion_of :payment, :in => [true, false]
+  validates_presence_of :transaction_at
+  validates_presence_of :amount
+  validates_numericality_of :amount, :greater_than => 0, :allow_nil => true
   validate :ensure_user_from_and_to_is_different
 
   def title
@@ -10,11 +13,15 @@ class Bill < ActiveRecord::Base
   end
 
   def action
-    "%s %s %s" % [self.from_user.name, self.verb, self.to_user.name]
+    "%s %s %s" % [self.from_user.name, self.past_verb, self.to_user.name]
+  end
+
+  def past_verb
+    self.payment? ? "payed" : "owes"
   end
 
   def verb
-    self.payment? ? "payed" : "owes"
+    self.payment? ? "pay" : "owe"
   end
 
   # turns the amount into a debt or a loan depending on the given user
@@ -48,6 +55,6 @@ class Bill < ActiveRecord::Base
   private
 
   def ensure_user_from_and_to_is_different
-    errors.add_to_base "From and to should be different" if from_user_id == to_user_id
+    errors.add_to_base "One can't really #{self.verb} oneself, can one?" if from_user_id == to_user_id
   end
 end
