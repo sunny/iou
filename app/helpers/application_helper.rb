7bffaa40e073f2821a3b823f5dc8ce7fa86e6f21
Sunny <negatif@gmail.com>
2008-08-18 00:29:42 +0200

    Handy helper to add a span markup for current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # Puts a span if the link is to the current page
+  def link_or_span(name, options = {}, html_options = {})
+    link_to_unless_current(name, options, html_options) do
+      content_tag(:span, name, html_options)
+    end
+  end
 end
