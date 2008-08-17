e0be658bf84a804f97304d9af210d8b88bf636d9
Sunny <negatif@gmail.com>
2008-08-17 14:05:30 +0200

    Basic actions and views for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def create
+    @user = User.create(params[:user])
+    if @user.errors.empty?
+      redirect_to users_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(params[:user])
+      redirect_to users_path
+    else
+      render :action => 'edit'
+    end
+  end
+
 end
