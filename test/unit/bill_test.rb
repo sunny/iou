b2b57cc34965880ccf0b7db9c6e5c08f4df2a1f0
Sunny Ripert <negatif@gmail.com>
2008-08-17 13:06:40 +0200

    Testing new Bill methods

--- test/unit/bill_test.rb
+++ test/unit/bill_test.rb
@@ -12,4 +12,20 @@ class BillTest < ActiveSupport::TestCase
     bill.to_user_id   = 1
     assert !bill.valid?, "From and to cannot not be the same"
   end
+
+  def test_find_all_by_user
+    sunny = users(:sunny)
+    assert_equal 4, Bill.find_all_by_user(sunny.id).size
+  end
+
+  def test_debt_and_loans_for_user
+    sunny = users(:sunny)
+    julie = users(:julie)
+
+    sunny_debts = Bill.debts_for_user(sunny)
+    julie_debts = Bill.debts_for_user(julie)
+
+    assert_equal -1.0, sunny_debts[julie.id] 
+    assert_equal  1.0, julie_debts[sunny.id]
+  end
 end
