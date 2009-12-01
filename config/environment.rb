18038a4011781f2954e11139ece6ce582a19ddb0
Sunny Ripert <sunny@sunfox.org>
2009-12-01 15:40:21 +0100

    Replaced haml plugin with `rake gems:install`

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ SALT = "b1b9ea35d65d220a11688c1ef051b7b58bde6de99d"
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "haml"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
