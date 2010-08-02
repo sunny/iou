8eac8c1cfaea0bcbe93eab9d27eea2898d54f945
Sunny Ripert <sunny@sunfox.org>
2010-08-02 23:29:38 +0200

    Added default action mailer hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ Iou::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'iou.heroku.com' }
+
   # Enable threaded mode
   # config.threadsafe!
 
