ff37f585ac6781f733d73a122604bec39339a1d7
Sunny <sunny@sunfox.org>
2010-08-17 00:45:56 +0200

    Bill form now with JS to switch text

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,96 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(window).load(function() {
+  // Text translations by language
+  var translations = {
+    en: {
+      bill: {
+        with_amount: {
+          with_user: {
+            bill: {
+              you_payed: "%s owes you %f €",
+              payed_you: "You owe %s %f €"
+            },
+            payment: {
+              you_payed: "%s payed you back %f €",
+              payed_you: "You gave %f € back to %s"
+            }
+          },
+          without_user: {
+            bill: {
+              you_payed: "Someone owes you %f €",
+              payed_you: "You owe someone %f €"
+            },
+            payment: {
+              you_payed: "Someone payed you back %f €",
+              payed_you: "You gave back %f € to someone"
+            }
+          }
+        },
+        without_amount: {
+          with_user: {
+            bill: {
+              you_payed: "%s owes you money",
+              payed_you: "You owe money to %s"
+            },
+            payment: {
+              you_payed: "%s payed you back",
+              payed_you: "You payed back %s"
+            }
+          },
+          without_user: {
+            bill: {
+              you_payed: "Someone owes you money",
+              payed_you: "You owe someone money"
+            },
+            payment: {
+              you_payed: "Someone payed you back",
+              payed_you: "You payed back someoe"
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  $('body').addClass('js')
+
+
+  // Bill form
+  var bill_form = $('.new_bill,.edit_bill')
+  if (bill_form.length) {
+    var bill_tabs = $('#bill_tabs label'),
+        friend_input = $('#friend_name'),
+        amount_input = $('#bill_amount'),
+        you_payed_text = $('#you_payed_true span'),
+        payed_you_text = $('#you_payed_false span')
+
+    // Returns the bill amount with two decimals
+    function bill_amount() {
+      var num = parseFloat(amount_input.val())
+      return num > 0 ? Math.round(num*100)/100 : 0
+    }
+
+    // Updates the tab selection and the payment text
+    function bill_form_update() {
+      var checked = bill_tabs.find(':checked'),
+          type = checked.attr('value'),
+          amount = bill_amount(),
+          user = friend_input.val(),
+          text = translations.en.bill
+            [amount > 0 ? 'with_amount' : 'without_amount']
+            [user ? 'with_user' : 'without_user']
+            [type.toLowerCase()]
+
+      bill_tabs.removeClass('checked')
+      checked.parent('label').addClass('checked')
+      you_payed_text.text(text.you_payed.replace(/%f/, amount).replace(/%s/, user))
+      payed_you_text.text(text.payed_you.replace(/%f/, amount).replace(/%s/, user))
+    }
+
+    // Add events on bill form
+    amount_input.blur(bill_form_update)
+    friend_input.blur(bill_form_update)
+    bill_form.click(bill_form_update)
+    bill_form_update()
+  }
+})
