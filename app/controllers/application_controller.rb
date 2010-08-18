dfad591acd1063ea75786a522150b42e1a76ce57
Sunny Ripert <sunny@sunfox.org>
2010-08-18 22:29:57 +0200

    Added 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
+class Error404 < StandardError
+end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
   before_filter :authenticate_user!
+
+  rescue_from Error404, :with => :render_404
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  protected
+
+  def render_404
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404.html", :status => '404 Not Found' }
+      format.xml  { render :nothing => true, :status => '404 Not Found' }
+    end
+  end
 end
